60dd26e9a825dac0c40a0398433f3ab41ae66b16
Marcin Kulik <marcin.kulik@gmail.com>
2009-11-10 23:51:26 +0100

    added manifest file for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 puts "Configuring production environment..."
 
+use Rack::LessCss, :less_path => File.join(APP_ROOT, "public", "stylesheets")
+
 # configure logging
 log_dir = File.join(APP_ROOT, 'log')
 FileUtils.mkdir_p(log_dir) unless File.exists?(log_dir)
--- config/init.rb
+++ config/init.rb
@@ -1,11 +1,14 @@
 APP_ROOT = File.join(File.dirname(__FILE__), '..')
 
 # load needed gems
-require File.join(APP_ROOT, 'vendor', 'gems', 'environment.rb')
-Bundler.require_env(ENV['RACK_ENV'] || 'development')
+#require File.join(APP_ROOT, 'vendor', 'gems', 'environment.rb')
+#Bundler.require_env(ENV['RACK_ENV'] || 'development')
+require 'sinatra'
+require 'dm-core'
+require 'rack-lesscss'
 
 # connect to db
-DataMapper.setup(:default, "sqlite3://#{File.expand_path(File.join(APP_ROOT, 'db', 'db.sqlite3'))}")
+#DataMapper.setup(:default, "sqlite3://#{File.expand_path(File.join(APP_ROOT, 'db', 'db.sqlite3'))}")
 
 # load lib/*
 %w(extensions.rb helpers.rb models.rb).each { |f| require File.join(APP_ROOT, 'lib', f) }
